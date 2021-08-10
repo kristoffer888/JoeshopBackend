efa8fd5fe782ff572cae3d477dd8b8f2437880c9
Rasmus Dal <dal@dr3.dk>
2021-08-10 09:23:55 +0200

    Created ORM Interface and implementatin class

---
+++ SimleShopORM/IORM.cs
@@ -0,0 +1,17 @@
+﻿using SimpleShopModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimleShopORM
+{
+    public interface IORM
+    {
+        public List<Customer> GetCustomers();
+        public Customer GetCustomer(int id);
+        public List<Product> GetProducts();
+        public Product GetProduct(int id);
+    }
+}
---
+++ SimleShopORM/ORM_MsSql.cs
@@ -0,0 +1,51 @@
+﻿using SimpleShopModels;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimleShopORM
+{
+    public class ORM_MsSql : IORM
+    {
+        private SqlConnection dbConn;
+        private string host;
+        private string username;
+        private string password;
+        private string database;
+
+        public ORM_MsSql()
+        {
+            SqlConnectionStringBuilder conString = new SqlConnectionStringBuilder()
+            {
+                InitialCatalog = database,
+                UserID = username,
+                Password = password,
+                DataSource = host
+
+            };
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
