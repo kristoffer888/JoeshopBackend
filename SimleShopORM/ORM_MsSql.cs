98915011553fc664a304ff2f5ce66ef17a45897a
Rasmus Dal <dal@dr3.dk>
2021-08-10 10:45:43 +0200

    Created Console test app and implemented Get Customer method in ORM

--- SimleShopORM/ORM_MsSql.cs
+++ SimleShopORM/ORM_MsSql.cs
@@ -11,10 +11,10 @@ namespace SimleShopORM
     public class ORM_MsSql : IORM
     {
         private SqlConnection dbConn;
-        private string host;
-        private string username;
-        private string password;
-        private string database;
+        private string host = "10.142.69.56";
+        private string username = "SimpleShop";
+        private string password = "SimpleShop";
+        private string database = "SimpleShop";
 
         public ORM_MsSql()
         {
@@ -26,11 +26,42 @@ namespace SimleShopORM
                 DataSource = host
 
             };
+
+            // Instantier objekt af SqlConnection
+            dbConn = new SqlConnection(conString.ToString());
         }
 
         public Customer GetCustomer(int id)
         {
-            throw new NotImplementedException();
+            Customer customer = null;
+
+            string query = "SELECT id, navn FROM kunde WHERE id = @val";
+            SqlCommand cmd = new SqlCommand(query, dbConn);
+            cmd.Parameters.AddWithValue("@val", id);
+
+            if (dbConn.State == System.Data.ConnectionState.Closed) 
+            {
+                try
+                {
+                    // open database connection
+                    dbConn.Open();
+                }
+                catch (Exception ex) {
+                    throw new Exception(ex.Message);
+                }
+
+                SqlDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                int i = 0;
+                while (reader.Read()) 
+                {
+                    customer = new Customer(reader.GetInt32(0), reader.GetString(1));
+                    i++;
+                }
+
+                if (i != 1) return null;
+            }
+            
+            return customer;
         }
 
         public List<Customer> GetCustomers()
---
+++ SimpleShopORMTestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using SimleShopORM;
+using SimpleShopModels;
+using System;
+
+namespace SimpleShopORMTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ORM_MsSql ORM = new ORM_MsSql();
+            Customer cust1 = ORM.GetCustomer(2);
+            Console.WriteLine(cust1.CustomerName);
+        }
+    }
+}
